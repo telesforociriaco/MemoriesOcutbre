586aeef5cd24c9d5b23fbb8edc41fd4a026abb30
nestorRabbin <rabbin_cbta@hotmail.com>
2018-10-29 19:10:27 -0600

    maquetacion de carta 29/10/18

--- src/app/components/letters-page/letters-page.component.ts
+++ src/app/components/letters-page/letters-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-letters-page',
   templateUrl: './letters-page.component.html',
-  styleUrls: ['../../app.component.scss']
+  styleUrls: ['../../app.component.scss', './letters-page.component.scss']
 })
 export class LettersPageComponent implements OnInit {
 
