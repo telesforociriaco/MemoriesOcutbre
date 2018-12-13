f1c6f710ebe9c4cbfc31b61a81f2a7878ea26de3
nestorRabbin <rabbin_cbta@hotmail.com>
2018-12-13 17:51:13 -0600

    acomodo de footer 1#/12/18

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['../home-page/home-page.component.scss','../home-page/css/swiper.css','../home-page/css/dark.css','../home-page/css/font-icons.css','../home-page/css/animate.css','../home-page/css/magnific-popup.css']
 })
 export class FooterComponent implements OnInit {
 
